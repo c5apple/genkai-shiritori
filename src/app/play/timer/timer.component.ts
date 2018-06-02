c4b3589fc87eaa0f35490f96140364ded9732e77
c5apple <c5apple_party@yahoo.co.jp>
2018-06-02 15:58:48 +0900

    残り時間表記

--- src/app/play/timer/timer.component.ts
+++ src/app/play/timer/timer.component.ts
@@ -14,14 +14,13 @@ import { TimerService } from '../../shared/service/timer.service';
 export class TimerComponent implements OnInit {
 
   /** 持ち時間 */
-  timeLimit: number = 15; // 15分
+  timeLimit: number = 900; // 15分
 
   /** 残り時間 */
   time: number = 0;
 
   /** タイマー */
   timer: Subscription;
-  // timer: Observable<number>;
 
   /** 停止中か */
   isStoped: boolean;
@@ -33,6 +32,23 @@ export class TimerComponent implements OnInit {
     this.time = this.timeLimit;
 
     this.start();
+  }
+
+  /**
+   * 残り時間表記
+   */
+  get timeStr(): string {
+    const h = this.time / 3600 | 0;
+    const m = this.time % 3600 / 60 | 0;
+    const s = this.time % 60 | 0;
+
+    if (h !== 0) {
+      return h + "時間" + `0${m}`.slice(-2) + "分" + `0${s}`.slice(-2) + "秒";
+    } else if (m !== 0) {
+      return m + "分" + `0${s}`.slice(-2) + "秒";
+    } else {
+      return s + "秒";
+    }
   }
 
   /**
