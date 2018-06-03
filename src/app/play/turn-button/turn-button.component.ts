749893ca9d644934b0b5d4cdbfb46939fa6b4678
c5apple <c5apple_party@yahoo.co.jp>
2018-06-03 21:14:41 +0900

    山札自動補充

--- src/app/play/turn-button/turn-button.component.ts
+++ src/app/play/turn-button/turn-button.component.ts
@@ -14,12 +14,16 @@ export class TurnButtonComponent implements OnInit {
   constructor(private cardService: CardService) { }
 
   ngOnInit() {
+    this.turn();
   }
 
   /**
    * 次へボタンクリック
    */
   public turn() {
+    if (this.cardService.deck.length === 0) {
+      this.cardService.shuffle();
+    }
     this.cardService.drowCard();
     this.cardService.discardCount = this.cardService.discardCount + 1;
   }
